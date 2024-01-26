adc53394701bcedc1baf4d24fb4bd534de2bc4d1
Kevin12-U <1719110155@utectulancingo.edu.mx>
2024-01-26 11:48:28 -0600

    desifrar texto

--- js/app.js
+++ js/app.js
@@ -11,3 +11,17 @@ function cifrar() {
   let parrafo = document.getElementById("resultado");
   parrafo.innerHTML = `${cifrado}`;
 }
+
+function desifrar() {
+  /* obtener el balor del text area */
+  let textoUsuario = document.getElementById("textoUsuario").value;
+  /* cambiar por un numero menor en base a su codigo ascci */
+  let desifrado = "";
+  for (let i = 0; i < textoUsuario.length; i++) {
+    let texto = textoUsuario.charCodeAt(i);
+    desifrado += String.fromCharCode(texto + 14);
+  }
+  /* mostrar como resultado en una etiqueta en html */
+  let parrafo = document.getElementById("resultado");
+  parrafo.innerHTML = `${desifrado}`;
+}
